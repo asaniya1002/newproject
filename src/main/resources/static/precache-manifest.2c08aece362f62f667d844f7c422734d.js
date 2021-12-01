94e19a3e2b75f697fc73bcef40b97120b0c86fc9
asaniya1002 <91761909+asaniya1002@users.noreply.github.com>
2021-12-02 04:43:26 +0530

    Adding static file to resources but not the Procfile yet....cause it seems troublesome

---
+++ src/main/resources/static/precache-manifest.2c08aece362f62f667d844f7c422734d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a99e265d18934a2e1800fed26ced6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99cfb7aded9f7ed4fb5d",
+    "url": "/static/css/2.13ec7091.chunk.css"
+  },
+  {
+    "revision": "fd8567d1cd7ebdeba2d1",
+    "url": "/static/css/main.67952568.chunk.css"
+  },
+  {
+    "revision": "99cfb7aded9f7ed4fb5d",
+    "url": "/static/js/2.14ca5553.chunk.js"
+  },
+  {
+    "revision": "7a38bb6e5a7060411ee65c9ad68817f5",
+    "url": "/static/js/2.14ca5553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553c696a18c9c5d23c32",
+    "url": "/static/js/3.d0acb1e2.chunk.js"
+  },
+  {
+    "revision": "fd8567d1cd7ebdeba2d1",
+    "url": "/static/js/main.84cf7827.chunk.js"
+  },
+  {
+    "revision": "a0d7b992a52009e9303f",
+    "url": "/static/js/runtime-main.d512cc42.js"
+  }
+]);
