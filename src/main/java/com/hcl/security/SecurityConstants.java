82bdddcda11149c0dfff456db020b825145fe54f
asaniya1002 <91761909+asaniya1002@users.noreply.github.com>
2021-12-01 14:10:37 -0800

    Update SecurityConstants.java
    Changing token expiration duration to 30 minutes

--- src/main/java/com/hcl/security/SecurityConstants.java
+++ src/main/java/com/hcl/security/SecurityConstants.java
@@ -8,5 +8,5 @@ public class SecurityConstants {
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 90_000; //where 30_000 is 30 seconds
+    public static final long EXPIRATION_TIME = 1800000; //where 30_000 is 30 seconds...changing to 30 mins for heroku
 }
